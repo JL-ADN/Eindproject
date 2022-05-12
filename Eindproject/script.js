63dd15e1adb0be5549a44c23c8967a8bb1c9fa85
Adrien <adrienguimard8@gmail.com>
2022-05-12 10:21:43 +0200

    menu verandering 10:21  12/05/22

--- Eindproject/script.js
+++ Eindproject/script.js
@@ -8,3 +8,14 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
